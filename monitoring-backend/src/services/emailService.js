0fc967e5fd68f200c0b9ae5ac7aced55a9acd68c
bernardo-dmartins <bernardo.dmartins7@gmail.com>
2026-06-04 00:13:17 -0300

    Fix: Email credentials

--- monitoring-backend/src/services/emailService.js
+++ monitoring-backend/src/services/emailService.js
@@ -19,7 +19,7 @@ class EmailService {
     try {
       this.transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST || "smtp.gmail.com",
-        port: Number(process.env.EMAIL_PORT) || 465,
+        port: Number(process.env.EMAIL_PORT) || 587,
         secure: Number(process.env.EMAIL_PORT) === 465,
         auth: {
           user: process.env.EMAIL_USER,
